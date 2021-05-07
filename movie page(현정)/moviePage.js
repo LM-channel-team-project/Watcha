d720622618a1a46908c84f52d9365a638217f1ed
hyunjeongPark <GuoHJP@gmail.com>
2021-05-07 12:03:42 +0900

    슬라이드 기능 구현 중

--- movie page(현정)/moviePage.js
+++ movie page(현정)/moviePage.js
@@ -2,22 +2,29 @@
 const leftArrow = document.querySelector('.fa-chevron-left');
 const appearanceProduction = document.querySelector('.appearanceProduction');
 const productionUl = document.querySelector('.production');
+const productionLi = document.querySelector('.productionList');
 const chevron = document.querySelector('.chevron');
 const body = document.querySelector('body');
 
-const number = 0;
+let count = 0;
 
 appearanceProduction.addEventListener('click', (e)=>{
     const target = e.target;
     if(target.classList.contains('fa-chevron-right')){
-        const num = number - 60.4;
-        productionUl.style.transform = `translateX(${num}rem)`;
+        count = count - 60.4;
+        
+        productionUl.style.transform = `translate(${count}rem)`;
     }
     if(target.classList.contains('fa-chevron-left')){
-        const num = number + 60.4;
-        productionUl.style.transform = `translateX(${num}rem)`;
+        count = count + 60.4;
+        productionUl.style.transform = `translate(${count}rem)`;
     }
 })
+
+console.log(appearanceProduction.clientWidth);
+
+
+
 
 productionUl.addEventListener('mouseover', ()=>{
     chevron.style.visibility = "visible";
